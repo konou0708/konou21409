6b8237ad49235b5f0a72c0314c471a11567ab9bc
konou0708 <103973949+konou0708@users.noreply.github.com>
2023-02-25 23:45:08 +0900

    Create ex7-9.js

---
+++ ex7-9.js
@@ -0,0 +1,11 @@
+function nazo5(){
+if(document.getElementById('answer5').value=='アーロンジャッジ'){
+ alert('あたり！');
+}
+else if(document.getElementById('answer5').value=='アーロンジャッジ'){
+ alert('あたり！');
+}
+else{
+ alert('はずれ!');
+ }
+}
