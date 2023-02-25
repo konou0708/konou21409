73ad02b6e2f3e69a0d0c44bbbf51ef993c5e6186
konou0708 <103973949+konou0708@users.noreply.github.com>
2023-02-26 08:45:27 +0900

    Update ex7-9.js

--- ex7-9.js
+++ ex7-9.js
@@ -2,7 +2,7 @@ function nazo5(){
 if(document.getElementById('answer5').value=='家系ラーメン'){
  alert('あたり！');
 }
-else if(document.getElementById('answer5').value=='究極の風邪薬'){
+else if(document.getElementById('answer5').value=='家系ラーメン'){
  alert('あたり！');
 }
 else{
